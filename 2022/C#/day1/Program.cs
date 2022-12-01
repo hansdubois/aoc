1a179ea696611869e5019b6a013182b2d88e5055
Hans Dubois <hansdub@gmail.com>
2022-12-01 16:40:16 +0100

    Year 2022 Day 2 C#

---
+++ 2022/C#/day1/Program.cs
@@ -0,0 +1,5 @@
+﻿var elves = File.ReadAllText(@"./input/calories.txt")
+    .Split("\n\n")
+    .Select(x => x.Split("\n").Aggregate(0, (acc, c) => acc + Int32.Parse(c))).OrderByDescending(value=>value).ToList();
+
+Console.WriteLine("Max: {0}, Three: {1}", elves.GetRange(0, 1).Sum(), elves.GetRange(0, 3).Sum());
