995ad2b6a23ebd89fc9d5a7521b76d0bea8fe43c
Hans Dubois <hansdub@gmail.com>
2022-12-18 11:23:34 +0100

    Day18

--- 2022/C#/day15-1/Program.cs
+++ 2022/C#/day15-1/Program.cs
@@ -6,13 +6,8 @@ class Program
 {
     public static void Main(string[] args)
     {
-        var watch = new System.Diagnostics.Stopwatch();
-        watch.Start();
-
         var regex = new Regex(
             @"Sensor at x=(?<SensorX>-?\d+), y=(?<SensorY>-?\d+): closest beacon is at x=(?<BeaconX>-?\d+), y=(?<BeaconY>-?\d+)");
-            
-        var y_location = 2000000;
 
         var points = File.ReadAllLines("input/input.txt").Select(line => regex.Match(line)).Select((matches) =>
         {
@@ -29,48 +24,98 @@ class Program
 
             return point;
         }).ToList();
-
-        var inRange = points.FindAll((point) =>
-        {
-            return (point.GetDistance() - Math.Abs(y_location - point.sensor.y) >= 0);
-        });
-
-        var notPossible = new List<int>();
-        var beacons = new List<int>();
-        foreach (var point in inRange)
-        {
-            var distanceToY = point.GetDistance() - Math.Abs(y_location - point.sensor.y);
-            
-            var leftX = point.sensor.x - distanceToY;
-            var rightX = point.sensor.x + distanceToY;
-
-            for (var i = leftX; i <= rightX; i++)
-            {
-                notPossible.Add(i);
-            }
-
-            if (point.beacon.y == y_location)
-            {
-                beacons.Add(point.beacon.y);
-            }
-            
-            Console.WriteLine(
-                "In range x: {0}, y: {1}, distance: {2}, distance to Y: {3}, left: {4}, right {5}", 
-                point.sensor.x, 
-                point.sensor.y, 
-                point.GetDistance(), 
-                distanceToY,
-                leftX,
-                rightX
-            );
-        }
         
-        Console.WriteLine(notPossible.Distinct().ToList().Count - beacons.Distinct().ToList().Count);
-        watch.Stop();
-        Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
+        solvePart1(points);
+        
+        var a = solvePart2(points);
+        Console.Write((4000000 * a.x) + a.y);
+        
     }
     
+    public static void solvePart1(List<Point> points)
+    {
+         var watch = new System.Diagnostics.Stopwatch();
+            watch.Start();
+        
+            var y_location = 2000000;
+            
+            var inRange = points.FindAll((point) =>
+            {
+                return (point.GetDistance() - Math.Abs(y_location - point.sensor.y) >= 0);
+            });
+
+            var notPossible = new List<int>();
+            var beacons = new List<int>();
+            foreach (var point in inRange)
+            {
+                var distanceToY = point.GetDistance() - Math.Abs(y_location - point.sensor.y);
+                
+                var leftX = point.sensor.x - distanceToY;
+                var rightX = point.sensor.x + distanceToY;
+
+                for (var i = leftX; i <= rightX; i++)
+                {
+                    notPossible.Add(i);
+                }
+
+                if (point.beacon.y == y_location)
+                {
+                    beacons.Add(point.beacon.y);
+                }
+                
+                Console.WriteLine(
+                    "In range x: {0}, y: {1}, distance: {2}, distance to Y: {3}, left: {4}, right {5}", 
+                    point.sensor.x, 
+                    point.sensor.y, 
+                    point.GetDistance(), 
+                    distanceToY,
+                    leftX,
+                    rightX
+                );
+            }
+            
+            Console.WriteLine(notPossible.Distinct().ToList().Count - beacons.Distinct().ToList().Count);
+            watch.Stop();
+            Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
+    }
+
+    public static Coordinate solvePart2(List<Point> points)
+    {
+        var grid = new Dictionary<int, Dictionary<int, string>>();
+        
+        foreach (var sensor in points)
+        {
+            var edgesToCheck = sensor.GetEdgeLocations();
+            
+            foreach (var edge in edgesToCheck)
+            {
+                if (!isPointInRangeOfAnySensor(edge, points.FindAll((point => point.sensor.x != sensor.sensor.x && point.sensor.y != sensor.sensor.y))))
+                { 
+                    return edge;
+                }   
+            }
+        }
+
+        return null;
+    }
     
+ 
+    private static bool isPointInRangeOfAnySensor(Coordinate pointToCheck, List<Point> allPoints)
+    {
+        foreach (var point in allPoints)
+        {
+            if (pointToCheck.x < 0 || pointToCheck.y < 0 || pointToCheck.y > 20 || pointToCheck.x > 20)
+            {
+                Console.WriteLine($"{pointToCheck.x} {pointToCheck.y}");
+                break; // Out of range
+            }
+                
+            if (point.isInSensorRange(pointToCheck))
+                return true;
+        }
+
+        return false;
+    }
 }
 
 class Point
@@ -82,6 +127,59 @@ class Point
     {
         return Math.Abs(sensor.x - beacon.x) + Math.Abs(sensor.y - beacon.y);
     }
+    
+    public List<Coordinate> GetEdgeLocations()
+    {
+        var locations = new List<Coordinate>();
+
+        var top = new Coordinate() {x = sensor.x, y = sensor.y - GetDistance() - 1}; 
+        var bottom = new Coordinate() {x = sensor.x, y = sensor.y + GetDistance() + 1};
+        var left = new Coordinate() {x = sensor.x - GetDistance(), y = sensor.y - 1};
+        var right = new Coordinate() {x = sensor.x + GetDistance(), y = sensor.y + 1};
+        
+        locations.Add(top);
+        locations.Add(bottom);
+        locations.Add(left);
+        locations.Add(right);
+        
+        createCoordinatesBetweenPoints(top, right, locations);
+        createCoordinatesBetweenPoints(left, top, locations);
+        createCoordinatesBetweenPoints(left, bottom, locations);
+        createCoordinatesBetweenPoints(bottom, right, locations);
+
+        return locations;
+    }
+
+    public bool isInSensorRange(Coordinate pointToCheck)
+    {
+        return CalculateManhattanDistance(sensor, pointToCheck) <= GetDistance();
+    }
+
+    private void createCoordinatesBetweenPoints(Coordinate start, Coordinate end, List<Coordinate> list)
+    {
+        var (left, right) = start.x <= end.x ? (start, end) : (end, start);
+        
+        if (left.y <= end.y)
+        {
+            for (var offset = 0; offset <= right.x - left.x; ++offset)
+            {
+                list.Add(new Coordinate() {x = left.x + offset, y =left.y + offset});
+            }
+        }
+        else
+        {
+            for (var offset = 0; offset <= right.x - left.x; ++offset)
+            {
+                list.Add(new Coordinate() {x = left.x + offset, y = left.y - offset});
+            }
+        }
+    }
+    
+    public static int CalculateManhattanDistance(Coordinate left, Coordinate right)
+    {
+        return Math.Abs(left.x - right.x) + Math.Abs(left.y - right.y);
+    }
+
 }
 
 class Coordinate
---
+++ 2022/C#/day18/Program.cs
@@ -0,0 +1,113 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+var cubes = File.ReadAllLines("input/sample.txt").Select((line) =>
+{
+    var coordinates = line.Split(",").Select(coord => Int32.Parse(coord)).ToList();
+
+    return new Cube(coordinates[0], coordinates[1], coordinates[2]);
+}).ToList();
+
+
+var neighbors = new List<(int x, int y, int z)> { (1, 0, 0), (-1, 0, 0), (0, 1, 0), (0, -1, 0), (0, 0, 1), (0, 0, -1) };
+
+var noNeigbours = 0;
+foreach (var cube in cubes)
+{
+    foreach (var (x, y, z) in neighbors)
+    {
+        var count = cubes.FindAll((searchCube) =>
+        {
+            return searchCube.X == cube.X + x && searchCube.Y == cube.Y + y && searchCube.Z == cube.Z + z;
+        }).Count;
+
+        if (count == 0)
+        {
+            noNeigbours++;
+        }
+    }
+}
+
+Console.WriteLine(noNeigbours);
+
+var cubesHashMap  = File.ReadAllLines("input/sample.txt").Select((line) =>
+{
+    var coordinates = line.Split(",").Select(coord => Int32.Parse(coord)).ToList();
+    
+    return (x: coordinates[0], y:coordinates[1], z:coordinates[2]);
+}).ToHashSet();
+
+var maxX = cubesHashMap.Max(x => x.x);
+var maxY = cubesHashMap.Max(y => y.y);
+var maxZ = cubesHashMap.Max(z => z.z);
+var minX = cubesHashMap.Min(x => x.x);
+var minY = cubesHashMap.Min(y => y.y);
+var minZ = cubesHashMap.Min(z => z.z);
+
+var leftRight = Enumerable.Range(minX, maxX + 1).ToList();
+var bottomTop = Enumerable.Range(minY, maxY + 1).ToList();
+var depth = Enumerable.Range(minZ, maxZ + 1).ToList();
+
+// Get cubes on the outside
+bool isOnOutside((int x, int y, int z) cube)
+{
+    if (cubesHashMap.Contains(cube))
+        return false;
+
+    var checkedSides = new List<(int x, int y, int z)>();
+    var queue = new Queue<(int x, int y, int z)> ();
+    
+    queue.Enqueue(cube);
+    
+    while(queue.Any())
+    {
+        var checkingCube = queue.Dequeue();
+        if (checkedSides.Contains(checkingCube)) continue;
+        
+        checkedSides.Add(checkingCube);
+        
+        if(!leftRight.Contains(checkingCube.x) || !bottomTop.Contains(checkingCube.y) || !depth.Contains(checkingCube.z))
+        {
+            // on the outside
+            return true;
+        }
+        
+        if(!cubesHashMap.Contains(checkingCube))
+        {
+            foreach(var (dx, dy, dz) in neighbors)
+            {
+                queue.Enqueue((checkingCube.x + dx, checkingCube.y + dy, checkingCube.z + dz));
+            }
+        }
+    }
+    return false;
+}
+
+var onOutside = 0;
+
+foreach(var (x, y, z) in cubesHashMap)
+{
+    foreach(var (dx, dy, dz) in neighbors)
+    {
+        if (isOnOutside((x + dx, y + dy, z + dz)))
+        {
+            onOutside++;
+        }
+    }
+}
+
+Console.WriteLine(onOutside);
+
+
+class Cube
+{
+    public int X { get; }
+    public int Y { get; }
+    public int Z { get; }
+
+    public Cube(int x, int y, int z)
+    {
+        X = x;
+        Y = y;
+        Z = z;
+    }
+}
