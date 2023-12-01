e44c76e8bea5fdf2876faf55eb5945766259babb
Hans Dubois <hansdub@gmail.com>
2023-12-01 08:35:05 +0100

    2023 Day 1

---
+++ 2023/AOC/Day01/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Text.RegularExpressions;
+
+
+var calibrationValues = File.ReadAllLines("input/input.txt");
+
+Regex regex = new Regex( @"\d");
+var part1 = 
+    File.ReadAllLines("input/input.txt")
+    .Select(x => regex.Matches(x))
+    .Select(matches => Int64.Parse($"{matches.First()}{matches.Last()}")).Sum();
+
+Console.WriteLine($"Part 1: {part1}");
+
+// Can't replace with the number for part two, to cater for eightwo2dfadf234
+var replacements = new Dictionary<string, string>()
+{
+    { "one", "o1e" },
+    { "two", "t2o" },
+    { "three", "t3e" },
+    { "four", "f4r" },
+    { "five", "f5e" },
+    { "six", "s6x" },
+    { "seven", "s7n" },
+    { "eight", "e8t" },
+    { "nine", "n9e" },
+    { "zero", "z0o" },
+};
+
+var partTwo = new long();
+
+foreach (var calibrationValue in calibrationValues)
+{
+    var correctedCalibrationValue = calibrationValue;
+    foreach (var replacement in replacements)
+    {
+        correctedCalibrationValue = correctedCalibrationValue.Replace(replacement.Key, replacement.Value);
+    }
+    
+    var matches = regex.Matches(correctedCalibrationValue);
+    var total = Int64.Parse($"{matches.First()}{matches.Last()}");
+    
+    partTwo += total;
+}
+
+Console.WriteLine(partTwo);
