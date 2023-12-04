b5d663772062ad44a0d5b75c4a89199413c8c712
Hans Dubois <hansdub@gmail.com>
2023-12-04 09:07:15 +0100

    Day 2 & Day 3

---
+++ 2023/AOC/Day02/Program.cs
@@ -0,0 +1,116 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text.RegularExpressions;
+
+var input = File.ReadAllLines("input/sample.txt");
+
+var possibleGames = new List<long>();
+
+foreach (var gameString in input)
+{
+    var reds = 0;
+    var blues = 0;
+    var greens = 0;
+    
+    var gameParts = gameString.Split(":");
+    var gameNumber = Int64.Parse(gameParts[0].Split(" ")[1].ToString().Replace(":", ""));
+
+    var cubeSets = gameParts[1].Split(";");
+
+    var possible = true;
+
+    foreach (var cubeSet in cubeSets)
+    {
+        var red = new Regex( @"(\d+) red");
+        var redMatch = red.Matches(cubeSet);
+
+        if (redMatch.Count > 0 && Int64.Parse(redMatch[0].ToString().Split(" ")[0]) > 12)
+        {
+            possible = false;
+            break;
+        }
+        
+        var blue = new Regex( @"\d+ blue");
+        var blueMatch = blue.Matches(cubeSet);
+        if (blueMatch.Count > 0 && Int64.Parse(blueMatch[0].ToString().Split(" ")[0]) > 14)
+        {
+            possible = false;
+            break;
+        }
+        
+        
+        var green = new Regex( @"\d+ green");
+        var greenMatch = green.Matches(cubeSet);
+        if (greenMatch.Count > 0 && Int64.Parse(greenMatch[0].ToString().Split(" ")[0]) > 13)
+        {
+            possible = false;
+            break;
+        }
+    }
+
+    if (possible)
+    {
+        possibleGames.Add(gameNumber);
+    }
+    
+}
+
+
+var total = new List<int>();
+
+
+foreach (var gameString in input)
+{
+    var reds = 0;
+    var blues = 0;
+    var greens = 0;
+    
+    var gameParts = gameString.Split(":");
+    var gameNumber = Int64.Parse(gameParts[0].Split(" ")[1].ToString().Replace(":", ""));
+
+    var cubeSets = gameParts[1].Split(";");
+
+    foreach (var cubeSet in cubeSets)
+    {
+        var red = new Regex( @"(\d+) red");
+        var redMatch = red.Matches(cubeSet);
+
+        if (redMatch.Count > 0)
+        {
+            var redNumber = Int32.Parse(redMatch[0].ToString().Split(" ")[0]);
+            if (redNumber > reds)
+            {
+                reds = redNumber;
+            }
+        }
+        
+        var blue = new Regex( @"\d+ blue");
+        var blueMatch = blue.Matches(cubeSet);
+        if (blueMatch.Count > 0)
+        {
+            var blueNumber = Int32.Parse(blueMatch[0].ToString().Split(" ")[0]);
+            if (blueNumber > blues)
+            {
+                blues = blueNumber;
+            }
+        }
+        
+        var green = new Regex( @"\d+ green");
+        var greenMatch = green.Matches(cubeSet);
+        if (greenMatch.Count > 0)
+        {
+            var greenNumber = Int32.Parse(greenMatch[0].ToString().Split(" ")[0]);
+            if (greenNumber > greens)
+            {
+                greens = greenNumber;
+            }
+        }
+    }
+    
+    total.Add(reds * blues * greens);
+
+    Console.WriteLine($"Game: {gameNumber} {reds * blues * greens}");
+}
+
+Console.WriteLine(total.Sum());
+
---
+++ 2023/AOC/Day03/Program.cs
@@ -0,0 +1,78 @@
+﻿var input = File.ReadAllLines("input/sample.txt");
+
+var numbers = new List<Number>();
+var symbols = new List<Symbol>();
+
+for (var row = 0; row < input.Length; row++)
+{
+    var currentNumber = new Number();
+    var digits = new List<int>();
+
+    for (var col = 0; col < input[row].Length; col++)
+    {
+        if (input[row][col] == '.')
+            continue;
+
+        if (int.TryParse(input[row][col].ToString(), out var digit))
+        {
+            digits.Add(digit);
+            if (digits.Count == 1)
+            {
+                currentNumber.Start = (row, col);
+            }
+
+            while (col < input[row].Length - 1  && int.TryParse(input[row][col + 1].ToString(), out digit))
+            {
+                digits.Add(digit);
+                col++;
+            }
+
+            currentNumber.End = (row, col);
+            currentNumber.Value = int.Parse(string.Join("", digits));
+            numbers.Add(currentNumber);
+            currentNumber = new Number();
+            digits.Clear();
+        }
+        else
+        {
+            symbols.Add(new Symbol
+            {
+                Value = input[row][col],
+                Position = (row, col)
+            });
+        }
+    }
+}
+
+var part1 = numbers
+    .Where(number => symbols.Any(symbol => 
+        Math.Abs(symbol.Position.Row - number.Start.Row) <= 1 
+        && symbol.Position.Column >= number.Start.Column - 1 
+        && symbol.Position.Column <= number.End.Column + 1))
+    .Sum(number => number.Value);
+
+var part2 = symbols
+    .Where(symbol => symbol.Value == '*')
+    .Select(symbol => numbers.Where(number => 
+        Math.Abs(symbol.Position.Row - number.Start.Row) <= 1 
+        && symbol.Position.Column >= number.Start.Column - 1 
+        && symbol.Position.Column <= number.End.Column + 1)
+        .ToArray())
+    .Where(gears => gears.Length == 2)
+    .Sum(gears => gears[0].Value * gears[1].Value);
+
+Console.WriteLine($"Part 1: {part1}");
+Console.WriteLine($"Part 2: {part2}");
+
+internal struct Number
+{
+    public int Value { get; set; }
+    public (int Row, int Column) Start { get; set; }
+    public (int Row, int Column) End { get; set; }
+}
+
+internal struct Symbol
+{
+    public char Value { get; set; }
+    public (int Row, int Column) Position { get; set; }
+}
