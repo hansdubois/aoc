e267afa485c197070a1f396b80e5eaa752ab702e
Hans Dubois <hansdub@gmail.com>
2023-12-06 06:32:31 +0100

    Day 6

---
+++ 2023/AOC/Day04/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+var input = File.ReadAllLines("input/input.txt");
+var cardPoints = 0.0;
+
+var cardList = new Dictionary<int, int>();
+var priceList = new Dictionary<int, int>();
+
+foreach (var line in input)
+{
+    var split = line.Split(':');
+    var gameNumber = Int32.Parse(split[0].Replace("   ", " ").Replace("  ", " ").Split(" ")[1]);
+    
+    var winningNumbers = split[1].Split('|')[0].Replace("  ", " ").Trim().Split(" ").Select(x => Int32.Parse(x)).ToList();
+    var cardNumbers = split[1].Split('|')[1].Replace("  ", " ").Trim().Split(" ").Select(x => Int32.Parse(x)).ToList();
+
+    var matchingNumbers = winningNumbers.Intersect(cardNumbers).Count();
+    
+    if (matchingNumbers > 0)
+    {
+        cardPoints += Math.Pow(2, matchingNumbers - 1 );
+        //Console.WriteLine(Math.Pow(2, matchingNumbers -1 ));    
+    }
+    
+    cardList.Add(gameNumber, matchingNumbers);
+    priceList.Add(gameNumber, 1);
+}
+
+foreach (var card in priceList)
+{
+    for (var times = 0; times < card.Value; times++)
+    {
+        if (cardList[card.Key] > 0)
+        {
+            for (var nextKey = card.Key + 1; nextKey < card.Key + cardList[card.Key] + 1; nextKey++)
+            {
+                priceList[nextKey] += 1;
+            }
+        }
+    }
+}
+
+
+Console.WriteLine("Part 1:" + cardPoints);
+Console.WriteLine("Part 2:" + priceList.Values.Sum());
+
---
+++ 2023/AOC/Day06/Program.cs
@@ -0,0 +1,38 @@
+﻿var races = new List<Race>()
+{
+    new Race(46, 358),
+    new Race(68, 1054),
+    new Race(98, 1807),
+    new Race(66, 1080),
+}.Select(x => x.WaysToBeatRecord()).Aggregate((a, x) => a * x);
+
+Console.WriteLine("Part 1: " + races);
+Console.WriteLine("Part 2: " + new Race(46689866, 358105418071080).WaysToBeatRecord());
+
+internal class Race
+{
+    public long Time { get; }
+    public long Distance { get; }
+    
+    public Race(long time, long distance)
+    {
+        this.Time = time;
+        this.Distance = distance;
+    }
+
+    public int WaysToBeatRecord()
+    {
+        var ways = 0;
+
+        for (var speed = 1; speed < Time; speed++)
+        {
+            // Does it beat the record within time?
+            if (speed * (Time - speed) > Distance)
+            {
+                ways++;
+            }
+        }
+
+        return ways;
+    }
+}
